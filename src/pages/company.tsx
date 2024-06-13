d338156ffc87d323d6f8ac041d83efe43791356f
clyk4541 <canny226@gmail.com>
2024-06-13 13:58:18 -0700

    correction

--- src/pages/company.tsx
+++ src/pages/company.tsx
@@ -1,7 +1,16 @@
-import { UserInquiryForm } from '@/components/form/Forms';
+import LandingLayout from '@/components/layout/LandingLayout';
+import PureTextLayout from '@/components/layout/PureTextLayout';
+import CompanyContent from '@/components/templates/CompanyContent';
 
 const Company = () => {
-  return <UserInquiryForm />;
+  return (
+    <LandingLayout>
+      <PureTextLayout
+        header={CompanyContent.header}
+        subheaders={CompanyContent.subheaders}
+      />
+    </LandingLayout>
+  );
 };
 
 export default Company;
